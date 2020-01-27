1cbd13c05c6234812470b55f6ead39a6cdda4a06
Jude Agagad <jude.agagad@boom.camp>
2020-01-27 13:39:30 +0800

    Lorem ipsum changed

--- src/components/invoices/components/invoice/Invoice.jsx
+++ src/components/invoices/components/invoice/Invoice.jsx
@@ -1,15 +1,27 @@
 import React from 'react';
 import { PanelHeader } from 'common_components';
 import InvoiceTable from './InvoiceTable';
-import { Grid } from '@material-ui/core';
+import { Grid, Divider } from '@material-ui/core';
 const Invoice = () => {
 	return (
 		<Grid container>
 			<Grid item lg={12}>
+
 				<PanelHeader
 					title="Invoice"
-					subTitle="Lorem ipsum dolor sit amet, consectetur adipisicing elit. Officia, expedita"
+					subTitle={
+						<div>
+							<br />
+							<span>Comming soon: </span>
+							<ul>
+								<li>Automated Invoicing of Perfect Pitch Customers.</li>
+								<li>Manual Invoicing for customers using non Perfect Pitch dialers </li>
+							</ul>
+						</div>
+					}
 				/>
+
+				<Divider />
 
 				<div className="mt-normal">
 					<InvoiceTable />
