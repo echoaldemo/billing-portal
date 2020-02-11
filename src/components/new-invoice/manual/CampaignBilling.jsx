4e62800b50162a875a392867b4d2f41d87b60a9d
Jude Agagad <jude.agagad@boom.camp>
2020-02-11 14:26:33 +0800

    collapse

--- src/components/new-invoice/manual/CampaignBilling.jsx
+++ src/components/new-invoice/manual/CampaignBilling.jsx
@@ -1,14 +1,18 @@
 import React, { useContext, useState, useEffect } from "react";
 import { makeStyles } from "@material-ui/core/styles";
-import Table from "@material-ui/core/Table";
-import TableBody from "@material-ui/core/TableBody";
-import TableCell from "@material-ui/core/TableCell";
-import TableContainer from "@material-ui/core/TableContainer";
-import TableHead from "@material-ui/core/TableHead";
-import TableRow from "@material-ui/core/TableRow";
-import Paper from "@material-ui/core/Paper";
+import {
+  Collapse,
+  Table,
+  TableBody,
+  TableCell,
+  TableContainer,
+  TableHead,
+  TableRow
+} from "@material-ui/core";
+import { ExpandMore, ExpandLess } from "@material-ui/icons";
 
 const CampaignBilling = ({ campaignDetails }) => {
+  const [rowCollapse, setRowCollapse] = useState(null);
   return (
     <div style={{ border: "solid 1px #F1F1F1" }}>
       <Table aria-label="simple table">
@@ -29,47 +33,106 @@ const CampaignBilling = ({ campaignDetails }) => {
             <TableCell align="right">
               <b>Amount</b>
             </TableCell>
-            <TableCell align="right">
-              <b>Action</b>
-            </TableCell>
+            <TableCell align="right">&nbsp;</TableCell>
           </TableRow>
         </TableHead>
         <TableBody>
-          {campaignDetails.map((el, i) => {
-            return (
-              <React.Fragment key={i}>
-                <TableRow>
-                  <TableCell>
-                    <b>{el.name}</b>
-                  </TableCell>
-                  <TableCell align="right">Billable Hourse</TableCell>
-                  <TableCell align="right">2</TableCell>
-                  <TableCell align="right">$33 23</TableCell>
-                  <TableCell align="right">$32.21</TableCell>
-                  <TableCell align="right">Add | Delete</TableCell>
-                </TableRow>
-                <TableRow>
-                  <TableCell>&nbsp;</TableCell>
-                  <TableCell align="right">DID Billing</TableCell>
-                  <TableCell align="right">2</TableCell>
-                  <TableCell align="right">$33 23</TableCell>
-                  <TableCell align="right">$32.21</TableCell>
-                  <TableCell align="right">&nbsp;</TableCell>
-                </TableRow>
-                <TableRow>
-                  <TableCell>&nbsp;</TableCell>
-                  <TableCell align="right">Performance</TableCell>
-                  <TableCell align="right">2</TableCell>
-                  <TableCell align="right">$33 23</TableCell>
-                  <TableCell align="right">$32.21</TableCell>
-                  <TableCell align="right">&nbsp;</TableCell>
-                </TableRow>
-              </React.Fragment>
-            );
-          })}
+          <TableRow>
+            <TableCell>
+              <b>adasd</b>
+            </TableCell>
+            <TableCell align="right">3</TableCell>
+            <TableCell align="right">2</TableCell>
+            <TableCell align="right">$33 23</TableCell>
+            <TableCell align="right">$32.21</TableCell>
+            <TableCell align="right">
+              <ExpandMore
+                onClick={() => {
+                  setRowCollapse(true);
+                }}
+              />
+            </TableCell>
+          </TableRow>
         </TableBody>
       </Table>
+      <Collapse in={rowCollapse}>
+        <Table>
+          <TableBody>
+            <TableRow>
+              <TableCell>
+                <b>adasd</b>
+              </TableCell>
+              <TableCell align="right">3</TableCell>
+              <TableCell align="right">2</TableCell>
+              <TableCell align="right">$33 23</TableCell>
+              <TableCell align="right">$32.21</TableCell>
+              <TableCell align="right">
+                <ExpandMore
+                  onClick={() => {
+                    setRowCollapse(1);
+                  }}
+                />
+              </TableCell>
+            </TableRow>
+          </TableBody>
+        </Table>
+      </Collapse>
     </div>
+  );
+};
+
+const RowDetails = ({ elementDetails, index, rowCollapse, setRowCollapse }) => {
+  const ShowExpand = () => {
+    return (
+      <React.Fragment>
+        {rowCollapse !== index ? (
+          <ExpandMore
+            onClick={() => {
+              setRowCollapse(index);
+            }}
+          />
+        ) : (
+          <ExpandLess
+            onClick={() => {
+              setRowCollapse(null);
+            }}
+          />
+        )}
+      </React.Fragment>
+    );
+  };
+
+  return (
+    <React.Fragment>
+      <TableRow>
+        <TableCell>
+          <b>{elementDetails.name}</b>
+        </TableCell>
+        <TableCell align="right">3</TableCell>
+        <TableCell align="right">2</TableCell>
+        <TableCell align="right">$33 23</TableCell>
+        <TableCell align="right">$32.21</TableCell>
+        <TableCell align="right">
+          <ShowExpand />
+        </TableCell>
+      </TableRow>
+      <Collapse in={rowCollapse === index} unmountOnExit>
+        <Table>
+          <TableRow>
+            <TableCell>
+              <b>{elementDetails.name}</b>
+            </TableCell>
+            <TableCell align="right">3</TableCell>
+            <TableCell align="right">2</TableCell>
+            <TableCell align="right">$33 23</TableCell>
+            <TableCell align="right">$32.21</TableCell>
+            <TableCell align="right">
+              <ShowExpand />
+            </TableCell>
+          </TableRow>
+        </Table>
+      </Collapse>
+    </React.Fragment>
   );
 };
 
