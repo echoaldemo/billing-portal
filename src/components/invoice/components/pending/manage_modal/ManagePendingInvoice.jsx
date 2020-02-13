8ee46a121b29479bed1b1a174c2c975297c52f18
Jules Ballaran <jules.ballaran@boom.camp>
2020-02-13 06:31:59 +0000

    fix edit invoice

--- src/components/invoice/components/pending/manage_modal/ManagePendingInvoice.jsx
+++ src/components/invoice/components/pending/manage_modal/ManagePendingInvoice.jsx
@@ -6,7 +6,7 @@ import { Divider, Button } from '@material-ui/core'
 import InvoiceDetails from './InvoiceDetails'
 import ManagePendingFooter from './components/ManagePendingFooter'
 import { patch } from 'utils/api'
-
+import { handleAmt } from './constVar'
 export default function ManagePendingInvoice() {
   const {
     state,
@@ -28,26 +28,151 @@ export default function ManagePendingInvoice() {
       payload: { updateLoading: true }
     })
     const { id, ...rest } = formState
-    const { services, litigator, merchant } = state.itemTable
+    const { services, litigator, merchant, tax } = state.itemTable
     const last = rest.Line[rest.Line.length - 1]
-
+    rest.Line = []
+    let totalAmt = 0
     for (let i = 0; i < Object.keys(services).length; i++) {
-      console.log(services[i], 'test')
+      const { billable, performance, did, name } = services[i]
+      if (billable.amt !== 0) {
+        rest.Line.push({
+          DetailType: 'SalesItemLineDetail',
+          Amount: (billable.qty || 1) * (billable.rate || 0),
+          SalesItemLineDetail: {
+            ItemRef: {
+              value: '21',
+              name: 'Billable hours'
+            },
+            TaxCodeRef: {
+              value: tax.code ? 'TAX' : 'NON'
+            },
+            Qty: parseFloat(billable.qty) || 1,
+            UnitPrice: parseFloat(billable.rate) || 0
+          },
+          Description: name
+        })
+      }
+      if (performance.amt !== 0) {
+        rest.Line.push({
+          DetailType: 'SalesItemLineDetail',
+          Amount: (performance.qty || 1) * (performance.rate || 0),
+          SalesItemLineDetail: {
+            ItemRef: {
+              value: '22',
+              name: 'Performance'
+            },
+            TaxCodeRef: {
+              value: tax.code ? 'TAX' : 'NON'
+            },
+            Qty: parseFloat(performance.qty) || 1,
+            UnitPrice: parseFloat(performance.rate) || 0
+          },
+          Description: name
+        })
+      }
+      if (did.amt !== 0) {
+        rest.Line.push({
+          DetailType: 'SalesItemLineDetail',
+          Amount: (did.qty || 1) * (did.rate || 0),
+          SalesItemLineDetail: {
+            ItemRef: {
+              value: '23',
+              name: 'Did'
+            },
+            TaxCodeRef: {
+              value: tax.code ? 'TAX' : 'NON'
+            },
+            Qty: parseFloat(did.qty) || 1,
+            UnitPrice: parseFloat(did.rate) || 0
+          },
+          Description: name
+        })
+      }
+
+      if (handleAmt(services[i]) === 0) {
+        rest.campaigns.splice(i, 1)
+      }
+      totalAmt += handleAmt(services[i])
     }
-    // patch(`/api/pending/edit/${id}`, rest)
-    //   .then(res => {
-    //     dispatch({
-    //       type: 'set-update-loading',
-    //       payload: { updateLoading: false }
-    //     })
-    //     dispatch({
-    //       type: 'set-selected-data',
-    //       payload: { selectedData: res.data }
-    //     })
-    //   })
-    //   .then(() => {
-    //     getPendingInvoicesData()
-    //   })
+    if (litigator.amt !== 0) {
+      rest.Line.push({
+        DetailType: 'SalesItemLineDetail',
+        Amount: (litigator.qty || 1) * (litigator.rate || 0),
+        SalesItemLineDetail: {
+          ItemRef: {
+            value: '24',
+            name: 'Litigator scrubbing'
+          },
+          TaxCodeRef: {
+            value: tax.code ? 'TAX' : 'NON'
+          },
+          Qty: parseFloat(litigator.qty) || 1,
+          UnitPrice: parseFloat(litigator.rate) || 0
+        }
+      })
+    }
+    if (litigator.amt !== 0) {
+      rest.Line.push({
+        DetailType: 'SalesItemLineDetail',
+        Amount: parseFloat(merchant) || 0,
+        SalesItemLineDetail: {
+          ItemRef: {
+            value: '25',
+            name: 'Merchant fees'
+          },
+          TaxCodeRef: {
+            value: tax.code ? 'TAX' : 'NON'
+          }
+        }
+      })
+    }
+    totalAmt += parseFloat(litigator.amt) + parseFloat(merchant)
+    if (tax.percentage !== 0) {
+      totalAmt += totalAmt * (tax.percentage / 100) - tax.percentage / 100
+    }
+
+    if (tax.code) {
+      rest.TxnTaxDetail = {
+        TxnTaxCodeRef: {
+          value: tax.code
+        },
+        TotalTax: totalAmt * (tax.percentage / 100) - tax.percentage / 100,
+        TaxLine: [
+          {
+            DetailType: 'TaxLineDetail',
+            Amount: tax,
+            TaxLineDetail: {
+              NetAmountTaxable: totalAmt,
+              TaxPercent: tax.percentage,
+              TaxRateRef: {
+                value: tax.taxrate
+              },
+              PercentBased: true
+            }
+          }
+        ]
+      }
+    } else {
+      rest.TxnTaxDetail = null
+    }
+    last.Amount = totalAmt
+    rest.total = totalAmt
+    rest.Line.push(last)
+
+    patch(`/api/pending/edit/${id}`, rest)
+      .then(res => {
+        dispatch({
+          type: 'set-update-loading',
+          payload: { updateLoading: false }
+        })
+        dispatch({
+          type: 'set-selected-data',
+          payload: { selectedData: res.data }
+        })
+      })
+      .then(() => {
+        getPendingInvoicesData()
+      })
   }
 
   const EditButton = () => {
