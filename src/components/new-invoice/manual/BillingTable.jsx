64babf413f0c4c7e0d44d7ac53434fc95c6c8ae1
Jomar L. Bandol <jomar.bandol@boom.camp>
2020-02-20 07:38:37 +0000

    Feat/duplicate

--- src/components/new-invoice/manual/BillingTable.jsx
+++ src/components/new-invoice/manual/BillingTable.jsx
@@ -2,7 +2,7 @@
 import React, { useContext, useState, useEffect } from "react";
 import { ManualInvoiceContext } from "context/ManualInvoiceContext";
 import CampaignBilling from "./CampaignBilling";
-const BillingTable = () => {
+const BillingTable = ({ duplicate }) => {
   const billingData = {
     billableHrsQty: "",
     billableHrsRate: "",
@@ -23,19 +23,127 @@ const BillingTable = () => {
     selectedCampaign,
     state,
     campaignDetails,
-    setCampaignDetails
+    setCampaignDetails,
+    additionalFee,
+    setAdditionalFee
   } = useContext(ManualInvoiceContext);
 
   const getAllCampaignDetails = () => {
-    const campaignsDetails = selectedCampaign.map(uuid => {
-      let filteredDetails = state.campaigns.filter(item => item.uuid === uuid);
+    const campaignsDetails = selectedCampaign.map((uuid, x) => {
+      let filteredDetails = state.campaigns.filter(
+        (item) => item.uuid === uuid
+      );
+      if (duplicate) {
+        return {
+          ...filteredDetails[0],
+          billableHrsQty: Boolean(
+            duplicate.Line.slice(0, -1).filter(
+              (l) => l.SalesItemLineDetail.ItemRef.value === "21"
+            )[x]
+          )
+            ? duplicate.Line.slice(0, -1).filter(
+                (l) => l.SalesItemLineDetail.ItemRef.value === "21"
+              )[x].SalesItemLineDetail.Qty
+            : "",
+          billableHrsRate: Boolean(
+            duplicate.Line.slice(0, -1).filter(
+              (l) => l.SalesItemLineDetail.ItemRef.value === "21"
+            )[x]
+          )
+            ? duplicate.Line.slice(0, -1).filter(
+                (l) => l.SalesItemLineDetail.ItemRef.value === "21"
+              )[x].SalesItemLineDetail.UnitPrice
+            : "",
+          billableHrsTotalAmount: "",
 
+          didQty: Boolean(
+            duplicate.Line.slice(0, -1).filter(
+              (l) => l.SalesItemLineDetail.ItemRef.value === "23"
+            )[x]
+          )
+            ? duplicate.Line.slice(0, -1).filter(
+                (l) => l.SalesItemLineDetail.ItemRef.value === "23"
+              )[x].SalesItemLineDetail.Qty
+            : "",
+          didRate: Boolean(
+            duplicate.Line.slice(0, -1).filter(
+              (l) => l.SalesItemLineDetail.ItemRef.value === "23"
+            )[x]
+          )
+            ? duplicate.Line.slice(0, -1).filter(
+                (l) => l.SalesItemLineDetail.ItemRef.value === "23"
+              )[x].SalesItemLineDetail.UnitPrice
+            : "",
+          didTotalAmount: "",
+
+          performanceQty: Boolean(
+            duplicate.Line.slice(0, -1).filter(
+              (l) => l.SalesItemLineDetail.ItemRef.value === "22"
+            )[x]
+          )
+            ? duplicate.Line.slice(0, -1).filter(
+                (l) => l.SalesItemLineDetail.ItemRef.value === "22"
+              )[x].SalesItemLineDetail.Qty
+            : "",
+          performanceRate: Boolean(
+            duplicate.Line.slice(0, -1).filter(
+              (l) => l.SalesItemLineDetail.ItemRef.value === "22"
+            )[x]
+          )
+            ? duplicate.Line.slice(0, -1).filter(
+                (l) => l.SalesItemLineDetail.ItemRef.value === "22"
+              )[x].SalesItemLineDetail.UnitPrice
+            : "",
+          performanceTotalAmount: ""
+        };
+      }
       return { ...filteredDetails[0], ...billingData };
     });
     setCampaignDetails(campaignsDetails);
   };
   useEffect(() => {
     getAllCampaignDetails();
+    if (duplicate) {
+      setAdditionalFee({
+        ...additionalFee,
+        merchantQty: Boolean(
+          duplicate.Line.slice(0, -1).filter(
+            (l) => l.SalesItemLineDetail.ItemRef.value === "25"
+          )[0]
+        )
+          ? duplicate.Line.slice(0, -1).filter(
+              (l) => l.SalesItemLineDetail.ItemRef.value === "25"
+            )[0].SalesItemLineDetail.Qty
+          : "",
+        merchantRate: Boolean(
+          duplicate.Line.slice(0, -1).filter(
+            (l) => l.SalesItemLineDetail.ItemRef.value === "25"
+          )[0]
+        )
+          ? duplicate.Line.slice(0, -1).filter(
+              (l) => l.SalesItemLineDetail.ItemRef.value === "25"
+            )[0].SalesItemLineDetail.UnitPrice
+          : "",
+        scrubbingQty: Boolean(
+          duplicate.Line.slice(0, -1).filter(
+            (l) => l.SalesItemLineDetail.ItemRef.value === "24"
+          )[0]
+        )
+          ? duplicate.Line.slice(0, -1).filter(
+              (l) => l.SalesItemLineDetail.ItemRef.value === "24"
+            )[0].SalesItemLineDetail.Qty
+          : "",
+        scrubbingRate: Boolean(
+          duplicate.Line.slice(0, -1).filter(
+            (l) => l.SalesItemLineDetail.ItemRef.value === "24"
+          )[0]
+        )
+          ? duplicate.Line.slice(0, -1).filter(
+              (l) => l.SalesItemLineDetail.ItemRef.value === "24"
+            )[0].SalesItemLineDetail.UnitPrice
+          : ""
+      });
+    }
   }, [selectedCampaign]);
 
   return (
