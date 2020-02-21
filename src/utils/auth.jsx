e1634ee5bc51b2ca24b3fe05a842cb1ff0324e84
Jules Ballaran <jules.ballaran@boom.camp>
2020-02-21 08:37:35 +0000

    fix connect qb

--- src/utils/auth.jsx
+++ src/utils/auth.jsx
@@ -8,25 +8,29 @@ const authorizeUri = () => {
   jsonBody.redirectUri = process.env.REACT_APP_REDIRECT_URI
 
   auth('/authUri', { json: jsonBody }).then(authUri => {
-    //Launch Popup using the JS window Object
-    var parameters = 'location=1,width=800,height=650'
-    parameters +=
-      ',left=' +
-      (global.screen.width - 800) / 2 +
-      ',top=' +
-      (global.screen.height - 650) / 2
-    var win = window.open(authUri.data, 'connectPopup', parameters)
-    var pollOAuth = window.setInterval(function() {
-      try {
-        if (win.document.URL.indexOf('code') !== -1) {
-          window.clearInterval(pollOAuth)
-          win.close()
-          window.location.reload()
-        }
-      } catch (e) {
-        console.log(e)
-      }
-    }, 100)
+    // Launch Popup using the JS window Object
+    // var parameters = 'location=1,width=800,height=650'
+    // parameters +=
+    //   ',left=' +
+    //   (global.screen.width - 800) / 2 +
+    //   ',top=' +
+    //   (global.screen.height - 650) / 2
+    // var win = window.open(authUri.data, 'connectPopup', parameters)
+    // var pollOAuth = window.setInterval(function() {
+    //   try {
+    //     if (win.document.URL.indexOf('code') !== -1) {
+    //       window.clearInterval(pollOAuth)
+    //       win.close()
+    //       window.location.reload()
+    //     }
+    //   } catch (e) {
+    //     console.log(e)
+    //   }
+    // }, 100)
+    window.open(
+      authUri.data,
+      '_blank' // <- This is what makes it open in a new window.
+    )
   })
 }
 
