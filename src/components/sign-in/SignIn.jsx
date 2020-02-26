fa2a259185b48628475c6fbab7a054ced74c1df9
Jude Agagad <jude.agagad@boom.camp>
2020-02-26 16:08:32 +0800

    User login design

--- src/components/sign-in/SignIn.jsx
+++ src/components/sign-in/SignIn.jsx
@@ -1,56 +1,82 @@
-import React, { useContext } from 'react'
-import { GoogleLogin } from 'react-google-login'
-import { get, post } from 'utils/api'
-import { StateContext } from 'context/StateContext'
-import { Grid } from '@material-ui/core'
-import bg from 'assets/billing-background.jpg'
-import './style/index.scss'
+import React, { useContext, useEffect } from "react";
+import { GoogleLogin } from "react-google-login";
+import { get, post } from "utils/api";
+import { StateContext } from "context/StateContext";
+import logo from "assets/pp_logo_transparent_bkgrd.png";
+import "./style/index.scss";
 const SignIn = ({ history }) => {
-  const { dispatch } = useContext(StateContext)
-
+  const { state, dispatch } = useContext(StateContext);
+  useEffect(() => {
+    if (state.userProfile.name) {
+      history.push("/overview");
+    }
+  }, []);
   const responseGoogle = async res => {
-    const find = await get(`/api/users/${res.googleId}`)
+    const find = await get(`/api/users/${res.googleId}`);
     if (find.data) {
       dispatch({
-        type: 'set-user-profile',
+        type: "set-user-profile",
         payload: { userProfile: find.data }
-      })
+      });
     } else {
-      res.profileObj.status = 'active'
-      post('/api/users/create', res.profileObj).then(res =>
+      res.profileObj.status = "active";
+      post("/api/users/create", res.profileObj).then(res =>
         dispatch({
-          type: 'set-user-profile',
+          type: "set-user-profile",
           payload: { userProfile: res.data }
         })
-      )
+      );
     }
-    console.log(res)
-    localStorage.setItem('gooleToken', res.tokenId)
-    history.push('/overview')
-  }
+    console.log(res);
+    localStorage.setItem("gooleToken", res.tokenId);
+    history.push("/overview");
+  };
 
   return (
-    <Grid container style={{ height: '100vh' }}>
-      <Grid item xs={6} style={{ backgroundImage: `url(${bg})` }}></Grid>
-      <Grid
-        className="login-container"
-        container
-        item
-        xs={6}
-        justify="center"
-        alignItems="center"
-      >
-        <GoogleLogin
-          className="LoginSignIn"
-          clientId="28861163542-0mub2dqtubvcjgun3n9vlrnkgfhgi7n4.apps.googleusercontent.com"
-          buttonText={<span>SIGN IN WITH GOOGLE</span>}
-          onSuccess={responseGoogle}
-          onFailure={responseGoogle}
-          cookiePolicy={'single_host_origin'}
-        />
-      </Grid>
-    </Grid>
-  )
-}
+    <div className="landing-page-wrapper">
+      <div className="left-component-container">
+        <div className="landing-component-wrapper">
+          <div className="logo-container">
+            <img src={logo} width={180} />
+          </div>
+          <div className="message-container">
+            <h2 className="message-header">Welcome to Billing Portal</h2>
+            <div>
+              <p className="message-text">
+                Laborum aliquip deserunt duis consequat laboris. Ut aliquip
+                ullamco tempor deserunt dolore culpa cillum.
+              </p>
+            </div>
+          </div>
+          <div className="render-component-container">
+            <div className="login-btn-container">
+              <GoogleLogin
+                className="LoginSignIn"
+                clientId="28861163542-0mub2dqtubvcjgun3n9vlrnkgfhgi7n4.apps.googleusercontent.com"
+                buttonText={
+                  <span style={{ textTransform: "none", fontWeight: 600 }}>
+                    Sign in with Google
+                  </span>
+                }
+                style={{
+                  width: 150,
+                  display: "flex",
+                  alignItems: "center"
+                }}
+                onSuccess={responseGoogle}
+                onFailure={responseGoogle}
+                cookiePolicy={"single_host_origin"}
+              />
+            </div>
+          </div>
+        </div>
+        <div className="landing-footer">
+          <span>All rights reserved, Perfect Pitch 2019</span>
+        </div>
+      </div>
+      <div className="right-component-container" />
+    </div>
+  );
+};
 
-export default SignIn
+export default SignIn;
