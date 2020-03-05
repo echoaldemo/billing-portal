1b7176b12f96c279e1b1cec6f277e35c6c12aa9c
Jules Ballaran <jules.ballaran@boom.camp>
2020-03-05 01:13:40 +0000

    fix logout issue

--- src/components/system-settings/SystemSettings.jsx
+++ src/components/system-settings/SystemSettings.jsx
@@ -1,17 +1,35 @@
-import React from "react";
-import "./style/index.scss";
-import VerticalTab from "./components/VerticalTab";
-import TabsOptions from "./TabsOptions";
-import TabPanelOptions from "./TabPanelOptions";
+import React, { useContext } from 'react'
+import './style/index.scss'
+import VerticalTab from './components/VerticalTab'
+import { TabsOptionsAdmin, TabsOptionsUser } from './TabsOptions'
+import { TabPanelOptionsAdmin, TabPanelOptionsUser } from './TabPanelOptions'
+import { StateContext } from 'context/StateContext'
+import { TableLoader } from 'common-components'
+
 const SystemSettings = () => {
+  const {
+    state: { userProfile }
+  } = useContext(StateContext)
   return (
     <div>
-      <VerticalTab
-        TabsOptions={TabsOptions}
-        TabPanelOptions={TabPanelOptions}
-      />
+      {userProfile.id ? (
+        <VerticalTab
+          TabsOptions={
+            userProfile.type === 'admin' ? TabsOptionsAdmin : TabsOptionsUser
+          }
+          TabPanelOptions={
+            userProfile.type === 'admin'
+              ? TabPanelOptionsAdmin
+              : TabPanelOptionsUser
+          }
+        />
+      ) : (
+        <div style={{ minHeight: '70vh' }}>
+          <TableLoader />
+        </div>
+      )}
     </div>
-  );
-};
+  )
+}
 
-export default SystemSettings;
+export default SystemSettings
--- src/components/system-settings/TabPanelOptions.jsx
+++ src/components/system-settings/TabPanelOptions.jsx
@@ -1,13 +1,14 @@
-import React from "react";
-import GeneralSettings from "./components/general-settings/GeneralSettings";
-import BillingProfile from "./components/billing-profile/BillingProfile";
-import UserManagement from "./components/user-management/UserManagement";
-import TrashBin from "./components/trash-bin/TrashBin";
-const TabPanelOptions = [
+import React from 'react'
+import GeneralSettings from './components/general-settings/GeneralSettings'
+import BillingProfile from './components/billing-profile/BillingProfile'
+import UserManagement from './components/user-management/UserManagement'
+import TrashBin from './components/trash-bin/TrashBin'
+const TabPanelOptionsAdmin = [
   <GeneralSettings />,
   <UserManagement />,
   <BillingProfile />,
   <TrashBin />
-];
+]
+const TabPanelOptionsUser = [<GeneralSettings />, <BillingProfile />]
 
-export default TabPanelOptions;
+export { TabPanelOptionsAdmin, TabPanelOptionsUser }
