38db34bae658a17cd5909570565262995297eafa
Jech <jericho.aldemo@boom.camp>
2020-03-16 16:59:45 +0800

    replace slack with trello

--- src/utils/api.jsx
+++ src/utils/api.jsx
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const baseUrl = "http://phdev.perfectpitchtech.com:8001";
+const baseUrl = "https://6so59hwb30.execute-api.us-east-1.amazonaws.com/dev";
 /* const baseUrl = "http://localhost:8001"; */
 const baseMock = "http://5e2fcff79c29c900145db496.mockapi.io";
 const req = {
