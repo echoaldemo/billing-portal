e4339cbb24bf6d27ee3e19dbbb7e4a54b33e7796
Jules Ballaran <jules.ballaran@boom.camp>
2020-03-20 11:56:48 +0800

    add fetch

--- src/context/ManualInvoiceContext.jsx
+++ src/context/ManualInvoiceContext.jsx
@@ -1,85 +1,85 @@
 /* eslint-disable */
-import React, { useReducer, useEffect, useState, useContext } from "react";
-import { StateContext } from "context/StateContext";
+import React, { useReducer, useEffect, useState, useContext } from 'react'
+import { StateContext } from 'context/StateContext'
 import {
   mockCampaigns,
   mockCompanies,
   company
-} from "../components/new-invoice/mock";
+} from '../components/new-invoice/mock'
 import {
   computeInt,
   services,
   formatDate,
   additionalFeeDetails
-} from "utils/func";
-import { post } from "utils/api";
-import { postLog } from "utils/time";
-const today = new Date();
+} from 'utils/func'
+import { post, getAPI } from 'utils/api'
+import { postLog } from 'utils/time'
+const today = new Date()
 const date =
-  today.getFullYear() + "-" + (today.getMonth() + 1) + "-" + today.getDate();
+  today.getFullYear() + '-' + (today.getMonth() + 1) + '-' + today.getDate()
 const start =
-  today.getFullYear() + "-" + (today.getMonth() + 1) + "-" + today.getDate();
-today.setMonth(today.getMonth() + 1);
+  today.getFullYear() + '-' + (today.getMonth() + 1) + '-' + today.getDate()
+today.setMonth(today.getMonth() + 1)
 const end =
-  today.getFullYear() + "-" + (today.getMonth() + 1) + "-" + today.getDate();
+  today.getFullYear() + '-' + (today.getMonth() + 1) + '-' + today.getDate()
 const addMonth =
-  today.getFullYear() + "-" + (today.getMonth() + 2) + "-" + today.getDate();
+  today.getFullYear() + '-' + (today.getMonth() + 2) + '-' + today.getDate()
 const addWeek =
   today.getFullYear() +
-  "-" +
+  '-' +
   (today.getMonth() + 1) +
-  "-" +
-  (today.getDate() + 7);
+  '-' +
+  (today.getDate() + 7)
 
 const initialState = {
   companies: [],
   campaigns: [],
   loading: false
-};
+}
 
 const initialFormState = {
   company: false,
   campaign: [],
-  billingType: "1",
+  billingType: '1',
   billingPeriod: { start, end },
-  taxation: " "
-};
+  taxation: ' '
+}
 
 const initialAdditionalFee = {
-  merchantQty: "",
-  merchantRate: "",
+  merchantQty: '',
+  merchantRate: '',
   merchantTax: true,
   merchantInvalid: false,
 
-  scrubbingQty: "",
-  scrubbingRate: "",
+  scrubbingQty: '',
+  scrubbingRate: '',
   scrubbingTax: true
-};
+}
 
-const ManualInvoiceContext = React.createContext();
+const ManualInvoiceContext = React.createContext()
 const ManualInvoiceProvider = ({ children }) => {
-  const { state: state2 } = useContext(StateContext);
-  const [formState, setFormState] = useState(initialFormState);
-  const [createLoading, setCreateLoading] = useState(false);
-  const [selectedCampaign, setSelectedCampaign] = useState([]);
-  const [billingFormState, setBillingFormState] = useState([]);
-  const [additionalFee, setAdditionalFee] = useState(initialAdditionalFee);
-  const [showCreateNew, setShowCreateNew] = useState(false);
-  const [campaignDetails, setCampaignDetails] = useState([]);
-  const [taxChecked, setTaxChecked] = useState(true);
-  const [openWarningModal, setOpenWarningModal] = useState(false);
+  const { state: state2 } = useContext(StateContext)
+  const [formState, setFormState] = useState(initialFormState)
+  const [createLoading, setCreateLoading] = useState(false)
+  const [selectedCampaign, setSelectedCampaign] = useState([])
+  const [billingFormState, setBillingFormState] = useState([])
+  const [additionalFee, setAdditionalFee] = useState(initialAdditionalFee)
+  const [showCreateNew, setShowCreateNew] = useState(false)
+  const [campaignDetails, setCampaignDetails] = useState([])
+  const [taxChecked, setTaxChecked] = useState(true)
+  const [openWarningModal, setOpenWarningModal] = useState(false)
   const mockTaxation = [
-    { code: "5", taxrate: "7", name: "Utah", percentage: 6.1 },
-    { code: "7", taxrate: "11", name: "Mexico", percentage: 16 }
-  ];
-  const [tax, setTax] = useState(6.1);
+    { code: '5', taxrate: '7', name: 'Utah', percentage: 6.1 },
+    { code: '7', taxrate: '11', name: 'Mexico', percentage: 16 }
+  ]
+  const [tax, setTax] = useState(6.1)
   const computeItemService = (qty = 0, rate = 0) => {
-    return qty * rate;
-  };
+    return qty * rate
+  }
 
   const getBalance = () => {
-    let total = 0;
-    billingFormState.forEach((item) => {
+    let total = 0
+    billingFormState.forEach(item => {
       total +=
         computeItemService(
           item.billableHrsQty,
@@ -91,69 +91,69 @@ const ManualInvoiceProvider = ({ children }) => {
           item.performanceQty,
           item.performanceRate,
           item.performanceTaxed
-        );
-    });
-    return total;
-  };
+        )
+    })
+    return total
+  }
   const getTaxableServices = () => {
-    let total = 0;
-    billingFormState.forEach((item) => {
+    let total = 0
+    billingFormState.forEach(item => {
       if (item.billableHrsTaxed) {
         total += computeItemService(
           item.billableHrsQty,
           item.billableHrsRate,
           item.billableHrsTaxed
-        );
+        )
       }
       if (item.didTaxed) {
-        total += computeItemService(item.didQty, item.didRate, item.didTaxed);
+        total += computeItemService(item.didQty, item.didRate, item.didTaxed)
       }
       if (item.performanceTaxed) {
-        total += computeItemService(item.performanceQty, item.performanceTaxed);
+        total += computeItemService(item.performanceQty, item.performanceTaxed)
       }
-    });
-    return total;
-  };
+    })
+    return total
+  }
   const getTaxableAdditionalFees = () => {
-    let total = 0;
+    let total = 0
     if (additionalFee.merchantTax) {
       total += computeItemService(
         additionalFee.merchantRate / 100,
         getBalance()
-      );
+      )
     }
     if (additionalFee.scrubbingTax) {
       total += computeItemService(
         additionalFee.scrubbingQty,
         additionalFee.scrubbingRate
-      );
+      )
     }
-    return total;
-  };
+    return total
+  }
   const computeBalanceDue = () => {
-    let newBalanceDue = 0;
-    console.log(computeTax(), computeTotal());
+    let newBalanceDue = 0
+    console.log(computeTax(), computeTotal())
 
-    newBalanceDue = parseFloat(computeTax()) + parseFloat(computeTotal());
+    newBalanceDue = parseFloat(computeTax()) + parseFloat(computeTotal())
 
-    return newBalanceDue;
-  };
+    return newBalanceDue
+  }
   const taxableTotal = () => {
-    return getTaxableAdditionalFees() + getTaxableServices();
-  };
+    return getTaxableAdditionalFees() + getTaxableServices()
+  }
   const computeTax = () => {
-    let taxed = 0;
+    let taxed = 0
     if (taxChecked) {
-      let totalBills = parseFloat(taxableTotal());
-      let totalTaxation = parseFloat(tax / 100);
+      let totalBills = parseFloat(taxableTotal())
+      let totalTaxation = parseFloat(tax / 100)
 
-      taxed = totalBills * totalTaxation;
+      taxed = totalBills * totalTaxation
     }
-    return parseFloat(taxed).toFixed(2);
-  };
+    return parseFloat(taxed).toFixed(2)
+  }
   const computeTotal = () => {
-    let total = 0;
-    let balanceTotal = parseFloat(getBalance());
+    let total = 0
+    let balanceTotal = parseFloat(getBalance())
 
     let totalAdditionalFee =
       computeItemService(
@@ -165,132 +165,147 @@ const ManualInvoiceProvider = ({ children }) => {
         additionalFee.scrubbingQty,
         additionalFee.scrubbingRate,
         additionalFee.scrubbingTax
-      );
-    total = parseFloat(balanceTotal) + parseFloat(totalAdditionalFee);
-    return parseFloat(total).toFixed(2);
-  };
+      )
+    total = parseFloat(balanceTotal) + parseFloat(totalAdditionalFee)
+    return parseFloat(total).toFixed(2)
+  }
 
   const [state, dispatch] = useReducer((state, action) => {
     switch (action.type) {
-      case "set-loading":
-        return { ...state, loading: action.payload.loading };
-      case "set-companies":
-        return { ...state, companies: action.payload.companies };
-      case "set-campaigns":
-        return { ...state, campaigns: action.payload.campaigns };
+      case 'set-loading':
+        return { ...state, loading: action.payload.loading }
+      case 'set-companies':
+        return { ...state, companies: action.payload.companies }
+      case 'set-campaigns':
+        return { ...state, campaigns: action.payload.campaigns }
       default:
-        return null;
+        return null
     }
-  }, initialState);
+  }, initialState)
   useEffect(() => {
-    getGeneralData();
-  }, []);
+    getGeneralData()
+  }, [])
   useEffect(() => {
-    const { start } = formState.billingPeriod;
-    let dt = new Date(start);
-    if (formState.billingType === "1") dt.setMonth(dt.getMonth() + 1);
-    else dt.setDate(dt.getDate() + 7);
+    const { start } = formState.billingPeriod
+    let dt = new Date(start)
+    if (formState.billingType === '1') dt.setMonth(dt.getMonth() + 1)
+    else dt.setDate(dt.getDate() + 7)
 
     const dueDate =
-      dt.getFullYear() + "-" + (dt.getMonth() + 1) + "-" + dt.getDate();
+      dt.getFullYear() + '-' + (dt.getMonth() + 1) + '-' + dt.getDate()
 
     setFormState({
       ...formState,
       billingPeriod: { ...formState.billingPeriod, end: dueDate }
-    });
-  }, [formState.billingType, formState.billingPeriod.start]);
-  const setActiveCampaigns = (uuid) => {
-    const filteredCampaigns = state.campaigns.filter((c) => c.company === uuid);
-    setFormState({ ...formState, campaign: filteredCampaigns });
-  };
-  const getGeneralData = () => {
-    dispatch({ type: "set-loading", payload: { loading: true } });
-    setTimeout(() => {
+    })
+  }, [formState.billingType, formState.billingPeriod.start])
+  const setActiveCampaigns = uuid => {
+    const filteredCampaigns = state.campaigns.filter(c => c.company === uuid)
+    setFormState({ ...formState, campaign: filteredCampaigns })
+  }
+  const getGeneralData = async () => {
+    dispatch({ type: 'set-loading', payload: { loading: true } })
+    try {
+      const { data: companies } = await getAPI('/identity/company/list')
       dispatch({
-        type: "set-companies",
-        payload: { companies: mockCompanies }
-      });
+        type: 'set-companies',
+        payload: { companies }
+      })
+      const { data: campaigns } = await getAPI('/identity/campaign/list')
       dispatch({
-        type: "set-campaigns",
-        payload: { campaigns: mockCampaigns }
-      });
-      dispatch({ type: "set-loading", payload: { loading: false } });
-    }, 500);
-  };
+        type: 'set-campaigns',
+        payload: { campaigns }
+      })
+      dispatch({ type: 'set-loading', payload: { loading: false } })
+    } catch (err) {
+      console.log(err)
+    }
+    // setTimeout(() => {
+    //   dispatch({
+    //     type: "set-companies",
+    //     payload: { companies: mockCompanies }
+    //   });
+    //   dispatch({
+    //     type: "set-campaigns",
+    //     payload: { campaigns: mockCampaigns }
+    //   });
+    //   dispatch({ type: "set-loading", payload: { loading: false } });
+    // }, 500);
+  }
 
   const generateLine = () => {
-    let newLine = [];
+    let newLine = []
 
     billingFormState.map((item, index) => {
       services.map((service, serviceIndex) => {
-        let { qty, rate, tax } = service;
+        let { qty, rate, tax } = service
         if (computeInt(item[qty], item[rate]) !== 0) {
           newLine.push({
             Amount: computeInt(item[qty], item[rate]),
             SalesItemLineDetail: {
-              TaxCodeRef: { value: item[tax] ? "TAX" : "NON" },
+              TaxCodeRef: { value: item[tax] ? 'TAX' : 'NON' },
               ItemRef: { name: service.name, value: service.value },
               Qty: item[qty],
               UnitPrice: item[rate]
             },
-            DetailType: "SalesItemLineDetail",
+            DetailType: 'SalesItemLineDetail',
             Description: item.name,
             LineNum: index + 1,
             Id: `${index + serviceIndex + 1}`
-          });
+          })
         }
-      });
-    });
+      })
+    })
     additionalFeeDetails.map((detail, detailIndex) => {
-      let { qty, rate, tax, value } = detail;
+      let { qty, rate, tax, value } = detail
 
       const total = computeInt(
-        value === "25" ? additionalFee[rate] : additionalFee[qty],
-        value === "25" ? getBalance() : additionalFee[qty]
-      );
+        value === '25' ? additionalFee[rate] : additionalFee[qty],
+        value === '25' ? getBalance() : additionalFee[qty]
+      )
 
       if (total !== 0) {
         newLine.push({
           Amount: parseFloat(total),
           SalesItemLineDetail: {
-            TaxCodeRef: { value: additionalFee[tax] ? "TAX" : "NON" },
+            TaxCodeRef: { value: additionalFee[tax] ? 'TAX' : 'NON' },
             ItemRef: { name: detail.name, value: detail.value },
-            Qty: value === "25" ? null : additionalFee[qty],
+            Qty: value === '25' ? null : additionalFee[qty],
             UnitPrice: parseFloat(additionalFee[rate])
           },
-          DetailType: "SalesItemLineDetail",
+          DetailType: 'SalesItemLineDetail',
           Description: detail.name,
           LineNum: newLine.length + detailIndex,
           Id: `${newLine.length + detailIndex}`
-        });
+        })
       }
-    });
+    })
 
     newLine.push({
-      DetailType: "SubTotalLineDetail",
+      DetailType: 'SubTotalLineDetail',
       Amount: computeBalanceDue(),
       SubTotalLineDetail: {}
-    });
+    })
 
-    return newLine;
-  };
+    return newLine
+  }
 
-  const sendToQuickbooks = (data) => {
-    setCreateLoading(true);
-    post("/api/invoice", data)
-      .then((res) => {
-        setCreateLoading(false);
-        setShowCreateNew(true);
+  const sendToQuickbooks = data => {
+    setCreateLoading(true)
+    post('/api/invoice', data)
+      .then(res => {
+        setCreateLoading(false)
+        setShowCreateNew(true)
       })
-      .catch((err) => {
-        console.log(err);
-      });
-  };
+      .catch(err => {
+        console.log(err)
+      })
+  }
 
   const saveAsDraft = (data, type) => {
     let newData = {
       ...data,
-      invoiceType: "Manual",
+      invoiceType: 'Manual',
       company: company(formState.company),
       campaigns: campaignDetails,
       startDate: formatDate(new Date(formState.billingPeriod.start)),
@@ -298,35 +313,35 @@ const ManualInvoiceProvider = ({ children }) => {
       total: computeBalanceDue(),
       billingType: formState.billingType,
       docNumber: Math.floor(Math.random() * 9999),
-      status: type === "approve" ? 2 : 0
-    };
-    let logData;
-    if (type === "approve") {
+      status: type === 'approve' ? 2 : 0
+    }
+    let logData
+    if (type === 'approve') {
       logData = {
-        type: "sent-invoice",
+        type: 'sent-invoice',
         description: `${state2.userProfile.name} issued an invoice to ${newData.company.name}.`
-      };
+      }
     } else {
       logData = {
-        type: "create-draft",
+        type: 'create-draft',
         description: `${state2.userProfile.name} created a draft for ${newData.company.name}.`
-      };
+      }
     }
-    setCreateLoading(true);
-    post("/api/create_pending", newData)
-      .then((res) => {
-        logData.invoiceId = res.data.id;
-        postLog(logData);
-        setCreateLoading(false);
-        setShowCreateNew(true);
+    setCreateLoading(true)
+    post('/api/create_pending', newData)
+      .then(res => {
+        logData.invoiceId = res.data.id
+        postLog(logData)
+        setCreateLoading(false)
+        setShowCreateNew(true)
       })
-      .catch((err) => {
-        console.log(err);
-      });
-  };
+      .catch(err => {
+        console.log(err)
+      })
+  }
   const createManualInvoice = (type, handleClose) => {
-    const taxDetail = mockTaxation.find((item) => item.percentage === tax);
-    let taxDetails = null;
+    const taxDetail = mockTaxation.find(item => item.percentage === tax)
+    let taxDetails = null
     if (taxChecked) {
       taxDetails = {
         TxnTaxCodeRef: {
@@ -335,7 +350,7 @@ const ManualInvoiceProvider = ({ children }) => {
         TotalTax: computeTax(),
         TaxLine: [
           {
-            DetailType: "TaxLineDetail",
+            DetailType: 'TaxLineDetail',
             Amount: computeTax(),
             TaxLineDetail: {
               NetAmountTaxable: taxableTotal(),
@@ -347,7 +362,7 @@ const ManualInvoiceProvider = ({ children }) => {
             }
           }
         ]
-      };
+      }
     }
     let data = {
       CustomerRef: {
@@ -360,40 +375,40 @@ const ManualInvoiceProvider = ({ children }) => {
       CustomerMemo: {
         value: `Wire/ACH Instructions:\nRouting 124301025\nAccount: 4134870\nBIC: AMFOUS51\nPeople's Intermountain Bank\n712 E Main St\nLehi, UT, 84043\nIf paying by wire, please include your\ncompany name in the memo.\n\nIf you have any questions or concerns about current or past invoices,\ncontact Tanner Purser directly at 801-805-4602`
       }
-    };
-    switch (type) {
-      case "approve":
-        sendToQuickbooks(data, handleClose);
-        saveAsDraft(data, type);
-        break;
-      case "draft":
-        saveAsDraft(data, type);
-        break;
-      default:
-        saveAsDraft(data, type);
-        break;
     }
-  };
+    switch (type) {
+      case 'approve':
+        sendToQuickbooks(data, handleClose)
+        saveAsDraft(data, type)
+        break
+      case 'draft':
+        saveAsDraft(data, type)
+        break
+      default:
+        saveAsDraft(data, type)
+        break
+    }
+  }
 
   const resetAllFormState = () => {
-    setFormState(initialFormState);
-    setBillingFormState([]);
-    setSelectedCampaign([]);
-  };
+    setFormState(initialFormState)
+    setBillingFormState([])
+    setSelectedCampaign([])
+  }
 
   const allChecked = () => {
-    const result = billingFormState.map((item) => {
+    const result = billingFormState.map(item => {
       return (
-        item["billableHrsTaxed"] ||
-        item["didTaxed"] ||
-        item["performanceTaxed"] ||
+        item['billableHrsTaxed'] ||
+        item['didTaxed'] ||
+        item['performanceTaxed'] ||
         additionalFee.merchantTax ||
         additionalFee.scrubbingTax
-      );
-    });
+      )
+    })
 
-    return result.some((val) => val === true);
-  };
+    return result.some(val => val === true)
+  }
 
   return (
     <ManualInvoiceContext.Provider
@@ -434,7 +449,7 @@ const ManualInvoiceProvider = ({ children }) => {
     >
       {children}
     </ManualInvoiceContext.Provider>
-  );
-};
+  )
+}
 
-export { ManualInvoiceProvider, ManualInvoiceContext };
+export { ManualInvoiceProvider, ManualInvoiceContext }
