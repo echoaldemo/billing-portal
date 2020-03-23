fdfb265725facc2d930ef14f163db437bc3fa05a
Jules Ballaran <jules.ballaran@boom.camp>
2020-03-23 08:42:53 +0000

    refactor overview fetching

--- src/components/overview/activity-logs/index.jsx
+++ src/components/overview/activity-logs/index.jsx
@@ -1,112 +1,112 @@
-import React, { useContext, useEffect } from "react";
-import { withRouter } from "react-router-dom";
-import { OverviewContext, OverviewProvider } from "context/OverviewContext";
-import { StateContext } from "context/StateContext";
+import React, { useContext, useEffect } from 'react'
+import { withRouter } from 'react-router-dom'
+import { OverviewContext } from 'context/OverviewContext'
+import { StateContext } from 'context/StateContext'
 import {
   Paper,
   Typography,
   Divider,
   CircularProgress as Loader
-} from "@material-ui/core";
-import Add from "@material-ui/icons/PostAdd";
-import Trash from "@material-ui/icons/Delete";
-import Delete from "@material-ui/icons/DeleteForever";
-import Edit from "@material-ui/icons/Edit";
-import Email from "@material-ui/icons/Email";
-import Check from "@material-ui/icons/Check";
-import Draft from "@material-ui/icons/Drafts";
-import Review from "@material-ui/icons/RateReview";
-import Duplicate from "@material-ui/icons/FileCopy";
-import Restore from "@material-ui/icons/Restore";
-import { Timeline, TimelineEvent } from "react-event-timeline";
-import { makeStyles } from "@material-ui/core/styles";
-import { NoResult } from "common-components";
-import { getFromNow, bubbleColor, getPronoun } from "./utils";
+} from '@material-ui/core'
+import Add from '@material-ui/icons/PostAdd'
+import Trash from '@material-ui/icons/Delete'
+import Delete from '@material-ui/icons/DeleteForever'
+import Edit from '@material-ui/icons/Edit'
+import Email from '@material-ui/icons/Email'
+import Check from '@material-ui/icons/Check'
+import Draft from '@material-ui/icons/Drafts'
+import Review from '@material-ui/icons/RateReview'
+import Duplicate from '@material-ui/icons/FileCopy'
+import Restore from '@material-ui/icons/Restore'
+import { Timeline, TimelineEvent } from 'react-event-timeline'
+import { makeStyles } from '@material-ui/core/styles'
+import { NoResult } from 'common-components'
+import { getFromNow, bubbleColor, getPronoun } from './utils'
 
 const useStyles = makeStyles(() => ({
   root: {
-    padding: "3px 0px 3px 0px"
+    padding: '3px 0px 3px 0px'
   },
   grid: {
-    display: "grid",
-    height: "100%",
-    alignItems: "center",
-    justifyItems: "center"
+    display: 'grid',
+    height: '100%',
+    alignItems: 'center',
+    justifyItems: 'center'
   },
   container: {
     width: 375,
-    height: "42vh",
-    overflowY: "scroll"
+    height: '42vh',
+    overflowY: 'scroll'
   }
-}));
+}))
 
-const ActivityLogsComponent = props => {
-  const classes = useStyles();
-  const { state, dispatch } = useContext(OverviewContext);
-  const { state: mainState } = useContext(StateContext);
+const ActivityLogs = props => {
+  const classes = useStyles()
+  const { state, dispatch } = useContext(OverviewContext)
+  const { state: mainState } = useContext(StateContext)
   useEffect(() => {
-    let temp = state.logs;
+    let temp = state.logs
     temp.map(e => {
       switch (e.type) {
-        case "create-draft":
-          e["icon"] = <Add />;
-          break;
-        case "sent-invoice":
-          e["icon"] = <Email />;
-          break;
-        case "delete-invoice":
-          e["icon"] = <Trash />;
-          break;
-        case "edit-invoice":
-          e["icon"] = <Edit />;
-          break;
-        case "approve-invoice":
-          e["icon"] = <Check />;
-          break;
-        case "mark-draft":
-          e["icon"] = <Draft />;
-          break;
-        case "mark-review":
-          e["icon"] = <Review />;
-          break;
-        case "duplicate-invoice":
-          e["icon"] = <Duplicate />;
-          break;
-        case "restore-invoice":
-          e["icon"] = <Restore />;
-          break;
-        case "delete-permanently":
-          e["icon"] = <Delete />;
-          break;
+        case 'create-draft':
+          e['icon'] = <Add />
+          break
+        case 'sent-invoice':
+          e['icon'] = <Email />
+          break
+        case 'delete-invoice':
+          e['icon'] = <Trash />
+          break
+        case 'edit-invoice':
+          e['icon'] = <Edit />
+          break
+        case 'approve-invoice':
+          e['icon'] = <Check />
+          break
+        case 'mark-draft':
+          e['icon'] = <Draft />
+          break
+        case 'mark-review':
+          e['icon'] = <Review />
+          break
+        case 'duplicate-invoice':
+          e['icon'] = <Duplicate />
+          break
+        case 'restore-invoice':
+          e['icon'] = <Restore />
+          break
+        case 'delete-permanently':
+          e['icon'] = <Delete />
+          break
         default:
-          break;
+          break
       }
 
-      return null;
-    });
+      return null
+    })
     dispatch({
-      type: "set-logs",
+      type: 'set-logs',
       payload: { logs: temp }
-    });
-  }, [state.logs]); // eslint-disable-line
+    })
+  }, [state.logs]) // eslint-disable-line
   const clickEvent = (id, type) => {
-    console.log(type);
-    if (type !== "delete-invoice") {
+    console.log(type)
+    if (type !== 'delete-invoice') {
       props.history.push({
-        pathname: "/invoices",
+        pathname: '/invoices',
         state: { invoiceId: id }
-      });
+      })
     } else {
-      localStorage.setItem("localTab", 3);
+      localStorage.setItem('localTab', 3)
       props.history.push({
         pathname: `/settings`,
         state: { invoiceId: id }
-      });
+      })
     }
-  };
+  }
   return (
     <Paper>
-      <Typography variant="subtitle2" style={{ padding: 19 }}>
+      <Typography variant='subtitle2' style={{ padding: 19 }}>
         ACTIVITY LOG
       </Typography>
       <Divider />
@@ -138,15 +138,15 @@ const ActivityLogsComponent = props => {
         )}
       </div>
     </Paper>
-  );
-};
+  )
+}
 
-const ActivityLogs = props => {
-  return (
-    <OverviewProvider>
-      <ActivityLogsComponent {...props} />
-    </OverviewProvider>
-  );
-};
+// const ActivityLogs = props => {
+//   return (
+//     <OverviewProvider>
+//       <ActivityLogsComponent {...props} />
+//     </OverviewProvider>
+//   );
+// };
 
-export default withRouter(ActivityLogs);
+export default withRouter(ActivityLogs)
--- src/components/overview/draft-invoices/DraftInvoices.jsx
+++ src/components/overview/draft-invoices/DraftInvoices.jsx
@@ -1,39 +1,25 @@
-import React, { useEffect, useState } from "react";
-import { Drafts } from "@material-ui/icons";
-import CardInfo from "../card-info";
-import { get } from "utils/api";
+import React, { useContext } from 'react'
+import { Drafts } from '@material-ui/icons'
+import CardInfo from '../card-info'
+import { OverviewContext } from 'context/OverviewContext'
 
 const DraftInvoices = () => {
-  const [pending, setPending] = useState([]);
-  const [all, setAll] = useState([]);
-  const [loading, setLoading] = useState(true);
-  useEffect(() => {
-    setLoading(true);
-    get("/api/pending/list").then((res) => {
-      setAll(res.data);
-      const pendingInvoices = res.data.filter(
-        (invoices) => invoices.status === 0
-      );
-      console.log(pendingInvoices);
-      setPending(pendingInvoices);
-      setLoading(false);
-    });
-  }, []);
-  const formatNumber = (num) => {
-    return num.toString().replace(/(\d)(?=(\d{3})+(?!\d))/g, "$1,");
-  };
+  const {
+    state: { loading, draft, all }
+  } = useContext(OverviewContext)
+  const formatNumber = num => {
+    return num.toString().replace(/(\d)(?=(\d{3})+(?!\d))/g, '$1,')
+  }
 
   return (
     <CardInfo
-      primaryLabel="Draft Invoices"
-      secondaryLabel={
-        formatNumber(pending.length) + " / " + formatNumber(all.length)
-      }
+      primaryLabel='Draft Invoices'
+      secondaryLabel={formatNumber(draft) + ' / ' + formatNumber(all)}
       loading={loading}
       status={0}
-      icon={() => <Drafts fontSize="large" style={{ marginRight: 20 }} />}
+      icon={() => <Drafts fontSize='large' style={{ marginRight: 20 }} />}
     />
-  );
-};
+  )
+}
 
-export default DraftInvoices;
+export default DraftInvoices
