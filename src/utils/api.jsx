6f428235d6c5bd9943af4cde94253035eeeeb4cd
Jericho Aldemo <jericho.aldemo@boom.camp>
2020-03-25 16:54:00 +0800

    fixed jules bug lol

--- src/utils/api.jsx
+++ src/utils/api.jsx
@@ -1,18 +1,29 @@
 import axios from "axios";
-
 // const baseUrl = "http://phdev.perfectpitchtech.com:8001";
 // const baseUrl = "http://localhost:3000";
 // const baseUrl = 'http://localhost:15001'
 const baseUrl = "https://6so59hwb30.execute-api.us-east-1.amazonaws.com/dev";
 const baseMock = "http://5e2fcff79c29c900145db496.mockapi.io";
+var bptoken = localStorage.getItem("bpToken");
 const req = {
   headers: {
     Authorization: "Token 4eb2825deaaf3764e8d471619809cbd4ab3a4567",
     "Content-Type": "application/json"
   }
 };
-
-const get = (endpoint, data) => axios.get(`${baseUrl}${endpoint}`, data);
+const checkToken = () => {
+  if (bptoken) {
+    axios.defaults.headers.common["Authorization"] = `token ${bptoken}`;
+  } else {
+    axios.defaults.headers.common[
+      "Authorization"
+    ] = `token ${localStorage.getItem("bpToken")}`;
+  }
+};
+const get = (endpoint, data) => {
+  checkToken();
+  return axios.get(`${baseUrl}${endpoint}`, data);
+};
 const post = (endpoint, data) => axios.post(`${baseUrl}${endpoint}`, data);
 const patch = (endpoint, data) => axios.patch(`${baseUrl}${endpoint}`, data);
 const remove = (endpoint, data) => axios.delete(`${baseUrl}${endpoint}`, data);
